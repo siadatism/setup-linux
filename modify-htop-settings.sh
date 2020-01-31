1da7bedeb6bb1af30a6b2d3f7e89a0d516b848ec
Seyed Mehran Siadati <siadatism@gmail.com>
2020-01-31 10:24:07 -0800

    Improve htoprc.

--- modify-htop-settings.sh
+++ modify-htop-settings.sh
@@ -15,13 +15,6 @@ else
 	cp -v $HTOP_CONFIG $HTOP_BACKUP_CONFIG
 fi
 
-sed -i -e 's/shadow_other_users=0/shadow_other_users=1/' \
-	-e 's/show_thread_names=0/show_thread_names=1/' \
-	-e 's/tree_view=0/tree_view=1/' \
-	-e 's/detailed_cpu_time=0/detailed_cpu_time=1/' \
-	-e 's/cpu_count_from_zero=0/cpu_count_from_zero=1/' \
-	-e 's/account_guest_in_cpu_meter=0/account_guest_in_cpu_meter=1/' \
-	"$HTOP_CONFIG"
-echo "Changes has been done."
+cp -v htoprc "$HTOP_CONFIG"
 
 exit 0
